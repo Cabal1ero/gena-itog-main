68eb6dd10331c0a2169e039427419eff5812f354
Cabal1ero <95655196+Cabal1ero@users.noreply.github.com>
2025-06-23 22:19:42 +0300

    fgfdg

--- russkaya_dusha/apps/accounts/apps.py
+++ russkaya_dusha/apps/accounts/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
+def create_superuser():
+    from django.contrib.auth.models import User
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='admin'
+    )
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
